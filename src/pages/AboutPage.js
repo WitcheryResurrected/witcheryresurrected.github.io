5b95379fc37ce5e41892a982f26d6f033ba9998e
MsRandom <ashley@msrandom.net>
2023-01-12 01:47:12 +0300

    Remove unused import

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './../styles/Home.css';
-import {Link} from "react-router-dom";
 
 const ASH_GITHUB = 'https://github.com/MsRandom';
 const ABUSED_GITHUB = 'https://github.com/abused';
