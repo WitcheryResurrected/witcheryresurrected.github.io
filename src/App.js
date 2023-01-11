59a64a6c5f58ca7451a40e358aa9488e9e789c24
MsRandom <ashley@msrandom.net>
2023-01-11 08:45:45 +0300

    Fix linter error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter, HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import {Route, Switch} from "react-router";
 import {NavbarComponent} from "./components/NavbarComponent";
 
